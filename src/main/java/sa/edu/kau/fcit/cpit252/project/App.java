250a1d12e6e61b7c9a6e2df783049ff5b40d27c7
progMes0 <malwakeel0002@stu.kau.edu.sa>
2026-04-27 01:38:13 +0300

    Implemented runnable functions

--- src/main/java/sa/edu/kau/fcit/cpit252/project/App.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/App.java
@@ -3,6 +3,15 @@ package sa.edu.kau.fcit.cpit252.project;
 public class App {
     public static void main(String[] args)
     {
+        // The client only interacts with the PersonalFinanceManager.
+        // They do not need to manually handle the subsystems or the factories.
+        PersonalFinanceManagement app = new PersonalFinanceManagement();
 
+        app.addIncome(6000, "Monthly Salary");
+        app.addIncome(450, "gift");
+        app.addExpense(1200, "Rent");
+        app.addExpense(150, "Groceries");
+
+        app.viewDashboard();
     }
 }
